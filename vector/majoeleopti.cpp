00cbf53261e3b8b60e646b8f8fe74e2f6f4e34ce
Akshatsah04 <iamakshatsah@gmail.com>
2025-01-30 12:44:51 +0530

    moore's voting algo

--- vector/majoeleopti.cpp
+++ vector/majoeleopti.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    
+    vector<int> giv = {1 , 2 , 2, 2 , 1 ,1 ,2 , 3 ,2 ,1 ,2 ,2};
+
+    int fre =0, ans=0;
+
+    for(int i=0 ; i<giv.size() ; i++){
+        if(fre==0){
+            ans=giv[i];
+        }
+        if(ans == giv[i]){
+            fre++;
+        }
+        else{
+            fre--;
+        }
+    }
+    cout<<ans;
+    return 0;
 }
