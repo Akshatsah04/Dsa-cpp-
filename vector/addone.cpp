7c83c9eb0df26dea3eebb98ae537fbe3f30800eb
Akshatsah04 <iamakshatsah@gmail.com>
2025-02-20 10:25:02 +0530

    still solving this Q

--- vector/addone.cpp
+++ vector/addone.cpp
@@ -4,48 +4,52 @@
 using namespace std;
 
 int main(){
-
     vector<int> digits;
-    digits[0]=1;
-    digits[1]=2;
-    digits[2]=3;
-    digits[3]=4;
-    digits[4]=5;
+    digits.push_back(1);
+    digits.push_back(2);
+    digits.push_back(3);
+    digits.push_back(4);
+    digits.push_back(5);
 
     int num=0;
-        bool wh = true;
+    bool wh = true;
 
+    for(int i=0;i<digits.size();i++){
+        if(digits[i]!=9){
+            wh= false;
+        }
+    }
+
+
+    if(wh==true){
+        digits[0]=1;
+        for(int i=1; i<digits.size() ;i++){
+            digits[i]=0;
+        }
+        digits.push_back(0);
+    }else{
+        for(int i=0; i<digits.size();i++){
+            int val =digits.size()-i-1;
+            float power=pow(10,val);
+            // cout<<power<<endl;
+            num+=digits[i]*power;
+        }
+        // cout<<num<<endl;
+        num=num+1;
+        // cout<<num<<endl;
         for(int i=0;i<digits.size();i++){
-            if(digits[i]!=9){
-                wh= false;
-            }
+            float power =pow(10,digits.size()-i-1);
+            int val3=static_cast<int>(power);
+            int val2= num/val3;
+            digits[i]=num/val3;
+            cout<<val2<<endl;
         }
-
-
-        if(wh==true){
-            digits[0]=1;
-            for(int i=1; i<digits.size() ;i++){
-                digits[i]=0;
-            }
-            digits.push_back(0);
-        }else{
-            for(int i=0; i<digits.size();i++){
-                int power=pow(10,i);
-                num =digits[i]*power;
-            }
-            num=num+1;
-            for(int i=0;i<digits.size();i++){
-                int power = pow(10,digits.size()-1);
-                digits[i]=num%power;
-            }
-
-        }
+    }
 
 
         
-    for (int i = 0; i < digits.size(); i++)
-    {
-        cout<<digits[i]<<" ";
-    }    
+    // for (int i = 0; i < digits.size(); i++){
+    //     cout<<digits[i]<<" ";
+    // } 
     return 0;
 }
