745e0222c2a2aa99a2ff928a7a49d048d3d6cc4e
Akshatsah04 <iamakshatsah@gmail.com>
2025-05-11 10:50:34 +0530

    h

--- test/qw1.cpp
+++ test/qw1.cpp
@@ -3,19 +3,9 @@
 using namespace std;
 
 int main(){
-    vector<int> tr;
-    int n;
-    cin>>n;
-    for(int i=0;i<n ; i++){
-        int a ;
-        cin>>a;
-        tr.push_back(a);
-
-    }
-    for(int i=0;i<n ; i++){
-        cout<<tr[i];
-
-    }
+   for(int i=0;i<100;i++){
+    cout<<"rcb is best\n";
+   }
     
 
     return 0;
