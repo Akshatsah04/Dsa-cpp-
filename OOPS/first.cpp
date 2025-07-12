6cf2575954a91e6d67f6893f29dece4d388d4bfc
Akshatsah04 <iamakshatsah@gmail.com>
2025-07-13 00:08:05 +0530

    oops

---
+++ OOPS/first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class teacher {
+public:     
+    string name;
+    int id;
+};
+
+int main(){
+    teacher t1;
+    t1.name="super";
+}
